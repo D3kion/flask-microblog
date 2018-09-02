db9a86abd8b2d349cd6493d4a045fb4f8d31436b
Dekion <dekion@protonmail.com>
2018-09-02 14:57:20 +0300

    Added password hashing in User model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
 
@@ -10,6 +11,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
